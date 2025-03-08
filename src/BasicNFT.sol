b527f0b1f817b98fb924bee25b9cd26b29eb05f0
mikosis123 <mikosis93@gmail.com>
2025-03-08 21:23:13 +0300

    NFT basic contract

---
+++ src/BasicNFT.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNFT is ERC721 {
+    uint256 private s_NFTCount;
+    mapping(uint256 => string) private s_tokenIdtoURI;
+
+    constructor() ERC721("Wagan NFT", "WAGA") {
+        s_NFTCount = 0;
+    }
+
+    function mintNFT(string memory tokenURI) public {
+        s_tokenIdtoURI[s_NFTCount] = tokenURI;
+        _safeMint(msg.sender, s_NFTCount);
+        s_NFTCount++;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        return s_tokenIdtoURI[tokenId]; // ✅ Correct: Uses tokenId
+    }
+}
